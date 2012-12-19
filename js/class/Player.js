c4f6e9f2c8e0f7190f9d1b5ebe5b3b985f1b0fb8
korzewski <korzewski@gmail.com>
2012-12-19 22:36:22 +0100

    clear content

--- js/class/Player.js
+++ js/class/Player.js
@@ -2,16 +2,15 @@ function Player(config){
 	this.img = config.img;
 	this.x = config.x;
 	this.y = config.y;
-	
 	this.speed = 10;
 	this.radius = 10;
 	
 	this.bitmap = new createjs.Bitmap(this.img, 30, 30);
 	this.bitmap.regX = this.bitmap.regY = this.radius;
 	
-	this.hitAreaa = new createjs.Shape();
-	this.hitAreaa.graphics.beginFill("#000").drawRect(-12, -12, 30, 30);
-	this.bitmap.hitArea = this.hitAreaa;
+	this.hitArea = new createjs.Shape();
+	this.hitArea.graphics.beginFill("#000").drawRect(-12, -12, 30, 30);
+	this.bitmap.hitArea = this.hitArea;
 	
 	this.updateBitmapPosition();
 
